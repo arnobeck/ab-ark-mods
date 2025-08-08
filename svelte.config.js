72d479b5ac3df38f1336959b8b4076ddd014a498
Arnaud BECKER <becker.arnaud@gmail.com>
2025-08-08 10:10:41 +0200

    d

--- svelte.config.js
+++ svelte.config.js
@@ -45,7 +45,7 @@ const config = {
 		}),
 
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH || '' : ''
 		},
 
 		prerender: {
